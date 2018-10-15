e435a0f2f1fc3555514652c57cdfc647d069d9ff
marlon <falconsoft.3d@gmail.com>
2018-10-14 23:36:56 -0300

    Commit message

--- 00/webpersonal/portfolio/models.py
+++ 00/webpersonal/portfolio/models.py
@@ -6,8 +6,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name ="Titulo")
     description = models.TextField(verbose_name ="Descripción")
     image = models.ImageField(blank=True, upload_to='project', default='')
+    link = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name ="Fecha de Creación")
     updated = models.DateTimeField(auto_now=True, verbose_name ="Fecha de Edición")
+    
 
     # Añadimos traduccion
     class Meta:
