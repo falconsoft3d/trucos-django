a5dd89c830cf1afcb8b3861d555422b17f6dca3d
marlon <falconsoft.3d@gmail.com>
2018-10-15 22:59:51 -0300

    05 Modelos y Migraciones

--- 00/refugio/apps/adopcion/models.py
+++ 00/refugio/apps/adopcion/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=70)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=12)
+    email = models.EmailField()
+    domicilio = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name ="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name ="Fecha de Edición")
+
+    class Meta:
+        verbose_name = "Persona"
+        verbose_name_plural = "Personas"
+        ordering = ["-created"]
+
+    # Para que muestre el nombre
+    def __str__(self):
+        return self.title
--- 00/refugio/apps/mascota/models.py
+++ 00/refugio/apps/mascota/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=10)
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField()
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name ="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name ="Fecha de Edición")
